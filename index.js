faca305e34b732efe40d3281c692c0febc12491a
erikwatson <championchap@gmail.com>
2022-08-09 11:15:04 +0100

    Configuring the code formatter

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-import './style.css';
+import './style.css'
 
 const thingsToLinkify = Array.from(
   document.querySelectorAll('[data-a11y-clickable]')
-);
+)
 
 const extractedLinks = thingsToLinkify.map((container) => {
-  const aTag = container.querySelector('a[data-a11y-target]');
+  const aTag = container.querySelector('a[data-a11y-target]')
   const otherATags = Array.from(container.querySelectorAll('a')).filter(
     (x) => !x.hasAttribute('data-a11y-target')
-  );
+  )
 
   return {
     container,
     link: aTag.href,
     aTag,
     otherATags,
-  };
-});
+  }
+})
 
 extractedLinks.forEach(({ container, link, aTag, otherATags }) => {
-  const prevBorderColor = container.style.borderColor;
-  const prevLinkColor = container.style.borderColor;
+  const prevBorderColor = container.style.borderColor
+  const prevLinkColor = container.style.borderColor
 
   const removeHighlights = () => {
-    container.style.borderColor = prevBorderColor;
-    aTag.style.color = prevLinkColor;
-  };
+    container.style.borderColor = prevBorderColor
+    aTag.style.color = prevLinkColor
+  }
 
   const applyHighlights = () => {
-    container.style.borderColor = 'orange';
-    aTag.style.color = 'orange';
-  };
+    container.style.borderColor = 'orange'
+    aTag.style.color = 'orange'
+  }
 
-  container.style.cursor = 'pointer';
+  container.style.cursor = 'pointer'
 
   container.addEventListener('click', (evt) => {
-    window.open(link, '_blank');
-  });
+    window.open(link, '_blank')
+  })
 
-  aTag.addEventListener('click', (evt) => evt.preventDefault());
+  aTag.addEventListener('click', (evt) => evt.preventDefault())
 
   otherATags.forEach((x) => {
     x.addEventListener('click', (evt) => {
-      evt.stopPropagation();
-    });
+      evt.stopPropagation()
+    })
 
     x.addEventListener('mouseenter', (evt) => {
-      removeHighlights();
-    });
+      removeHighlights()
+    })
 
     x.addEventListener('mouseleave', (evt) => {
-      applyHighlights();
-    });
-  });
+      applyHighlights()
+    })
+  })
 
   container.addEventListener('mouseenter', (evt) => {
-    applyHighlights();
-  });
+    applyHighlights()
+  })
 
   container.addEventListener('mouseleave', (evt) => {
-    removeHighlights();
-  });
-});
+    removeHighlights()
+  })
+})
